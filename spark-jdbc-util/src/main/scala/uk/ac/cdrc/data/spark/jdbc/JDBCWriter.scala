9ab0f2b02dfebde8d037be489a9d54811a259974
Wen Li <spacelis@gmail.com>
2016-11-25 11:34:38 +0000

    Add overwrite option to write dataframes to tables

--- spark-jdbc-util/src/main/scala/uk/ac/cdrc/data/spark/jdbc/JDBCWriter.scala
+++ spark-jdbc-util/src/main/scala/uk/ac/cdrc/data/spark/jdbc/JDBCWriter.scala
@@ -16,6 +16,21 @@ import org.postgresql.core.BaseConnection
 
 case class JDBCWriter(jdbcUrl: String, connProps: Map[String, String]){
   //jdbcUrl = s"jdbc:postgresql://..." // db credentials elided
+  val connectionProperties: Properties = {
+    val props = new java.util.Properties()
+
+    props.setProperty("driver", "org.postgresql.Driver")
+    for { (k, v) <- connProps}
+      props.setProperty(k, v)
+
+    props
+  }
+
+  // Spark reads the "driver" property to allow users to override the default driver selected, otherwise
+  // it picks the Redshift driver, which doesn't support JDBC CopyManager.
+  // https://github.com/apache/spark/blob/v1.6.1/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala#L44-51
+  val cf: () => Connection = JdbcUtils.createConnectionFactory(jdbcUrl, connectionProperties)
+
   // Convert every partition (an `Iterator[Row]`) to bytes (InputStream)
   def rowsToInputStream(rows: Iterator[Row], delimiter: String): InputStream = {
 
@@ -43,30 +58,39 @@ case class JDBCWriter(jdbcUrl: String, connProps: Map[String, String]){
   )
 
   def schemaToTable(frame: DataFrame, table: String): String = {
-    val colDef = for {
-      (name, typ, nullable, _) <- frame.schema.fields()
-    } yield s"$name ${getSQLType(typ)}" mkString ", "
-    s"CREATE TABLE $table (${colDef});"
+    val colDef = (for {
+      (name, typ) <- frame.dtypes
+    } yield s"$name ${getSQLType(typ)}") mkString ", "
+    s"CREATE TABLE IF NOT EXISTS $table ($colDef);"
   }
 
-  def write(frame: DataFrame, table: String):Unit = {
-    val connectionProperties: Properties = {
-      val props = new java.util.Properties()
+  def dropTable(table: String): Unit = {
+    val conn = cf()
+    val stmt = conn.createStatement()
+    stmt.execute(s"DROP TABLE IF EXISTS $table")
+    conn.close()
+  }
 
-      props.setProperty("driver", "org.postgresql.Driver")
-      for { (k, v) <- connProps}
-        props.setProperty(k, v)
+  def deleteData(table: String): Int = {
+    val conn = cf()
+    val stmt = conn.createStatement()
+    val cnt = stmt.executeUpdate(s"DELETE from $table")
+    conn.close()
+    cnt
+  }
 
-      props
-    }
+  def createTable(frame: DataFrame, table: String): Unit = {
+    val conn = cf()
+    val stmt = conn.createStatement()
+    stmt.execute(schemaToTable(frame, table))
+    conn.close()
+  }
 
-    // Spark reads the "driver" property to allow users to override the default driver selected, otherwise
-    // it picks the Redshift driver, which doesn't support JDBC CopyManager.
-    // https://github.com/apache/spark/blob/v1.6.1/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala#L44-51
-    val cf: () => Connection = JdbcUtils.createConnectionFactory(jdbcUrl, connectionProperties)
+  def write(frame: DataFrame, table: String, overwrite: Boolean = false): Unit = {
 
-    val stmt = cf().createStatement()
-    stmt.executeUpdate(schemaToTable(frame, table))
+    if (overwrite)
+      dropTable(table)
+    createTable(frame, table)
 
     // Beware: this will open a db connection for every partition of your DataFrame.
     frame.foreachPartition { rows =>
